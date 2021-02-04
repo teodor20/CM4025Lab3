60db490740a135daa093f4921206012e33cf637d
teodor.narliyski <teodor.narliyski@gmail.com>
2021-02-04 16:28:08 +0000

    Added express as a dependency and html file

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let http = require("http");
+let fs = require('fs')
 
 const PORT = process.env.PORT || 3000;
 
@@ -6,11 +7,15 @@ http.createServer(function (request, response) {
     // Send the HTTP header
     // HTTP Status: 200: OK
     // Content Type: text/plain
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.writeHead(200, { 
+        'Content-Type': 'text/plain',
+        'Access-Control-Allow-Origin': '*'
+    });
 
+    let readStream = fs.createReadStream(__dirname + '/index.html');
     //Send the response body as "Hello World"
-    response.end('Hello World\n');
+    readStream.pipe(response);
 }).listen(PORT, () => {
     // Console will print the message
-    console.log('Server running at http://127.0.0.1:8080');
+    console.log('Server running at http://127.0.0.1:' + PORT);
 });
