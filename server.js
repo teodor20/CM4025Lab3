34c128ce8491af21a2f2f6db05d73953ece5d5ea
teodor.narliyski <teodor.narliyski@gmail.com>
2021-02-04 16:51:48 +0000

    Added middleware

--- server.js
+++ server.js
@@ -14,6 +14,16 @@ app.get('/', function(req, res) {
 // create routes for the admin section
 //get an instance of the router
 var adminRouter = express.Router();
+
+///////////////////////////////////////////////////////////////////
+// route middleware that will happen on every request
+adminRouter.use(function(req, res, next) {
+    // log each request to the console
+    console.log(req.method, req.url);
+    // continue doing what we were doing and go to the route
+    next(); });
+///////////////////////////////////////////////////////////////////
+
 // admin main page. the dashboard (http://localhost:PORT/admin)
 adminRouter.get('/', function(req, res) {
  res.send('I am the dashboard!'); });
