e312bc3fa3523f8c3a23e47fbf0fac5f36a8b1ff
teodor.narliyski <teodor.narliyski@gmail.com>
2021-02-04 16:58:19 +0000

    Added route with params

--- server.js
+++ server.js
@@ -33,6 +33,11 @@ adminRouter.get('/users', function(req, res) {
 // posts page (http://localhost:PORT/admin/posts)
 adminRouter.get('/posts', function(req, res) {
  res.send('I show all the posts!'); });
+
+// route with parameters (http://localhost:PORT/admin/users/:name)
+adminRouter.get('/users/:name', function(req, res) {
+  res.send('hello ' + req.params.name + '!'); }); 
+
 // apply the routes to our application
 app.use('/admin', adminRouter);
 ///////////////////////////////////////////////////////////////////
